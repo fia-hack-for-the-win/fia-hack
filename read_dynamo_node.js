6bff36d12ec9d3ebc56dd041e35343a17076bb1c
Avi Wilensky <avi@promediacorp.com>
2016-11-28 16:52:58 -0800

    node js lambda to read dnyamo db

---
+++ read_dynamo_node.js
@@ -0,0 +1,57 @@
+'use strict';
+
+console.log('Loading function');
+
+const doc = require('dynamodb-doc');
+
+const dynamo = new doc.DynamoDB();
+
+
+/**
+ * Provide an event that contains the following keys:
+ *
+ *   - operation: one of the operations in the switch statement below
+ *   - tableName: required for operations that interact with DynamoDB
+ *   - payload: a parameter to pass to the operation being performed
+ * {
+  "payload": {"Key":{"zipcode":"29409"}},
+  "operation": "read",
+  "tableName": "fia_notification_details"
+}
+ */
+exports.handler = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    const operation = event.operation;
+    const payload = event.payload;
+
+    if (event.tableName) {
+        payload.TableName = event.tableName;
+    }
+
+    switch (operation) {
+        case 'create':
+            dynamo.putItem(payload, callback);
+            break;
+        case 'read':
+            dynamo.getItem(payload, callback);
+            break;
+        case 'update':
+            dynamo.updateItem(payload, callback);
+            break;
+        case 'delete':
+            dynamo.deleteItem(payload, callback);
+            break;
+        case 'list':
+            dynamo.scan(payload, callback);
+            break;
+        case 'echo':
+            callback(null, payload);
+            break;
+        case 'ping':
+            callback(null, 'pong');
+            break;
+        default:
+            callback(new Error(`Unrecognized operation "${operation}"`));
+    }
+};
